a7951ec788b07553223b986347b196c210f81400
Jayesh <jayeshkumawat0803@gmail.com>
2026-09-16 00:07:30 +0530

    performed functions like loc and iloc on the dataframe

--- dataframe_basic.py
+++ dataframe_basic.py
@@ -12,8 +12,18 @@ df = pd.DataFrame(student_dict)
 print("Student Data Table")
 print(df)
 
+top_students = df[df["Marks"]>85]
+print(f"The Top Student who  has more than 85 marks are:\n {top_students}")
+print("-"*40)
 
+#postion based selectionn according to the index
+first_student = df.iloc[1:3]
+print(first_student)
 
+#extractinng buy lables using loc function
+print("-"*40)
+name_and_marks = df.loc[:,["Name","Marks"]]
+print(name_and_marks)
 
 
 
