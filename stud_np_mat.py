c9899d35d8f176971c8b9b4987c661fed8e1b17a
Jayesh <jayeshkumawat0803@gmail.com>
2026-09-25 23:58:40 +0530

    student data of marks in subjects using array finding avg and hisest in subjects

---
+++ stud_np_mat.py
@@ -0,0 +1,18 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+student = np.array(["Jayesh","Sogani","Vivek","Prashant","Uday","Pandu","Charchit","thapa","adi"])
+
+maths = np.array([100,90,87,68,54,49,87,68,89])
+phy = np.array([79,56,78,46,98,67,70,83,58])
+cs = np.array([90,56,75,86,46,97,67,39,79])
+
+avg_maths = np.mean(maths)
+print(f"Avverage Marks of Students in Maths: {avg_maths}")
+print('-'*40)
+
+high_phy = np.argmax(phy)
+print(f"Highest marks in Physics : {student(high_phy)}  {high_phy}")
+
+high_cs = np.argmax(cs)
+print(f"Highest marks is Computer Science: {student(high_phy)}  {high_phy}")
